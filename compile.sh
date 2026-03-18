add39d355f27712d54b29976e175c881f08d0d38
Mike Frysinger <vapier@gmail.com>
2026-03-18 03:53:46 -0400

    compile with mingw for Windows if available
    Since the code compiles cleanly for Windows via the mingw toolchain,
    add it to the compile script for quick checking.

--- compile.sh
+++ compile.sh
@@ -11,3 +11,7 @@ set -ex
 : "${GLIB_LIBS:=$(${PKG_CONFIG} glib-2.0 gio-2.0 --cflags --libs)}"
 ${CC} ${CFLAGS} ${WFLAGS} ${CPPFLAGS} GGGG.c copyfile.c ${GLIB_LIBS} -o GGGG
 ${CXX} ${CXXFLAGS} ${WFLAGS} ${CPPFLAGS} GGGG.cpp copyfile.c ${GLIB_LIBS} -o GGGG-cpp
+if command -v i686-w64-mingw32-gcc >/dev/null; then
+  i686-w64-mingw32-gcc ${CFLAGS} ${WFLAGS} ${CPPFLAGS} -U_FORTIFY_SOURCE GGGG.c copyfile.c -o GGGG.exe
+  i686-w64-mingw32-g++ ${CXXFLAGS} ${WFLAGS} ${CPPFLAGS} -U_FORTIFY_SOURCE GGGG.cpp copyfile.c -o GGGG-cpp.exe
+fi
