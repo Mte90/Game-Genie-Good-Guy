c15841e4f411feb820a150c49a3ac1e259d5fb21
Mike Frysinger <vapier@gmail.com>
2026-03-18 03:52:08 -0400

    hoist most compiler settings to variables
    This makes it easier to share/reuse with multiple compiler runs.

--- compile.sh
+++ compile.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 set -ex
-gcc -pipe -Wall -Os -Wformat -Wunused-variable -D_FORTIFY_SOURCE=2 -std=c17 GGGG.c copyfile.c $(pkg-config glib-2.0 gio-2.0 --cflags --libs) -o GGGG
-g++ -pipe -Wall -Os -Wformat -D_FORTIFY_SOURCE=2 -std=c++17 GGGG.cpp copyfile.c $(pkg-config glib-2.0 gio-2.0 --cflags --libs) -o GGGG-cpp
+: "${CC:=gcc}"
+: "${CXX:=g++}"
+: "${WFLAGS:=-Wall -Wformat -Wunused-variable}"
+: "${OPT:=-Os}"
+: "${CFLAGS:=${OPT} -pipe -std=c17}"
+: "${CXXFLAGS:=${OPT} -pipe -std=c++17}"
+: "${CPPFLAGS:=-D_FORTIFY_SOURCE=2}"
+: "${PKG_CONFIG:=pkg-config}"
+: "${GLIB_LIBS:=$(${PKG_CONFIG} glib-2.0 gio-2.0 --cflags --libs)}"
+${CC} ${CFLAGS} ${WFLAGS} ${CPPFLAGS} GGGG.c copyfile.c ${GLIB_LIBS} -o GGGG
+${CXX} ${CXXFLAGS} ${WFLAGS} ${CPPFLAGS} GGGG.cpp copyfile.c ${GLIB_LIBS} -o GGGG-cpp
