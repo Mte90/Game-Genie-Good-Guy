b70b9b431c65650df37e84aec0bd583aee2af3c7
Mike Frysinger <vapier@gmail.com>
2026-03-19 22:33:23 -0400

    fix Bin implementation
    This was using the itoa helper with a radix of 2, but that helper
    ignored the radix, so it never actually worked.  Implement it.

--- GGGG.c
+++ GGGG.c
@@ -281,8 +281,12 @@ char * join(int n, ...)
 
 char* Bin(int number)
 {
-    char *strtmp = BCX_TmpStr( 2048, 1, 1);
-    sprintf(strtmp, "%d", number);
+    char *strtmp = BCX_TmpStr( 32, 1, 1);
+    int i;
+    for (i = 0; i < 32; ++i)
+    {
+        strtmp[i] = (number & (1 << (31 - i))) ? '1' : '0';
+    }
     return strtmp;
 }
 
